ebdde876ad56adb946717c085c537beb406dd165
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-02-10 22:02:11 +0530

    added prime react updated readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { Avatar } from 'primereact/avatar';
+import { Badge } from 'primereact/badge';
+
+
 function App() {
   return (
-    <div> Hello world</div>
+    <div>
+      <Avatar className="p-overlay-badge" shape="circle" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} icon="pi pi-user" size="xlarge">
+        <Badge value="4" />
+      </Avatar>
+    </div>
   );
 }
 
