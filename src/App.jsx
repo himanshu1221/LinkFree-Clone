9df8c4e6561e64531ed54f8e56139f878e5df4f1
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-02-12 19:59:30 +0530

    modified to jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { Avatar } from 'primereact/avatar';
+import { Badge } from 'primereact/badge';
+import 'primeflex/primeflex.css';
+
+function App() {
+  return (
+    <div>
+      <div className="flex justify-content-center align-item-center">
+        <Avatar className="p-overlay-badge" shape="circle" style={{ backgroundColor: '#9c27b0', color: '#ffffff' }} icon="pi pi-user" size="xlarge" image="himanshu.jpg">
+          <Badge value="4" />
+        </Avatar>
+      </div>
+      <div className="flex justify-content-center align-item-center">
+        <p>Web Developer</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
