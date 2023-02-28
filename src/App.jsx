870889f4d183f22ffd9d7ae89f97fc3af6aa6ef5
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-02-28 23:03:52 +0530

    added beta buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import { Avatar } from 'primereact/avatar';
-import { Badge } from 'primereact/badge';
-import 'primeflex/primeflex.css';
+import { Avatar } from "primereact/avatar";
+import { Badge } from "primereact/badge";
+import "primeflex/primeflex.css";
+import { Button } from "primereact/button";
 
 function App() {
   return (
     <div className="m-4">
       <div className="flex justify-content-center align-item-center">
-        <Avatar className="p-overlay-badge" shape="circle" size="xlarge" image="himanshu.jpg">
+        <Avatar
+          className="p-overlay-badge"
+          shape="circle"
+          size="xlarge"
+          image="himanshu.jpg"
+        >
           <Badge value="4" />
         </Avatar>
-      </div>
+        </div>
       <div className="flex justify-content-center align-item-center">
         <p>Web Developer</p>
       </div>
+        <div className="flex flex-column justify-content-center align-item-center">
+        <Button className="p-3">
+          <i className="pi pi-twitter px-2"></i>
+          <span className="px-2">Twitter</span>
+        </Button>
+        <Button className="p-3">
+          <i className="pi pi-twitter px-2"></i>
+          <span className="px-2">Twitter</span>
+        </Button>
+        </div>
     </div>
   );
 }
