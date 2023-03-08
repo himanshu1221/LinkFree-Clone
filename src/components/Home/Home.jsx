aed7092e8e1ba8943b99278aadc6eb3a8d4af938
Pranav Tripathi <tripathipranav14@gmail.com>
2023-03-08 22:25:50 +0530

    add home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css'
+import { Navbar } from '../Navbar'
+
+const Home = () => {
+    return(
+        <div className='main-container'>
+            <Navbar />
+            <div className='top'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
