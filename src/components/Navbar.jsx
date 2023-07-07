1774f09139c4a74497247bd156a1343d57bd30f3
Denys Sichka <sickadenis@gmail.com>
2023-07-07 13:19:27 +0300

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import SearchBar from "./SearchBar";
-import "./navbar.css";
+import React from 'react'
+import SearchBar from './SearchBar'
+import './navbar.css'
 
 export const Navbar = () => {
-  const [visible, setVisible] = React.useState(false);
-  const [widthScreen, setWidthScreen] = React.useState(window.innerWidth);
-  const [searchResults, setSearchResults] = React.useState([]);
-  
+  const [visible, setVisible] = React.useState(false)
+  const [widthScreen, setWidthScreen] = React.useState(window.innerWidth)
+  const [searchResults, setSearchResults] = React.useState([])
+
   let darkMode = localStorage.getItem('darkMode')
 
   const enableDarkMode = () => {
@@ -25,87 +25,101 @@ export const Navbar = () => {
     localStorage.setItem('darkMode', null)
   }
 
-  if(darkMode === 'enabled'){
-    enableDarkMode();
+  if (darkMode === 'enabled') {
+    enableDarkMode()
   }
 
   const darkModeToggle = () => {
-    darkMode = localStorage.getItem('darkMode');
+    darkMode = localStorage.getItem('darkMode')
 
-    if(darkMode !== 'enabled'){
-      enableDarkMode();
-    }
-    else{
-      disableDarkMode();
+    if (darkMode !== 'enabled') {
+      enableDarkMode()
+    } else {
+      disableDarkMode()
     }
   }
 
   const handleSearch = (searchTerm) => {
     window.open(
       `https://linkfree-clone.vercel.app/${searchTerm}`,
-      "_blank",
-      "noreferrer"
-    );
-  };
+      '_blank',
+      'noreferrer'
+    )
+  }
 
-  const toggleShowBar = () => setVisible(!visible);
+  const toggleShowBar = () => setVisible(!visible)
 
   const handleResize = () => {
-    setWidthScreen(window.innerWidth);
-  };
+    setWidthScreen(window.innerWidth)
+  }
 
   React.useEffect(() => {
     if (visible) {
-      setVisible(false);
+      setVisible(false)
     }
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize)
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, [widthScreen]);
+    return () => window.removeEventListener('resize', handleResize)
+  }, [widthScreen])
 
   return (
-    <ul className="parent_nav" style={{ background: "#1a1a1b" }}>
+    <ul className='parent_nav' style={{ background: '#1a1a1b' }}>
       {!visible ? (
-        <ul
-          className="links"
-          style={{ listStyle: "none", background: "#1a1a1b" }}
-        >
-          <SearchBar onSearch={handleSearch} />
-          <a href="https://linkfree.eddiehub.io/events">
-            <li>Events</li>
-          </a>
-          <a href="https://linkfree.eddiehub.io/map">
-            <li>Map</li>
-          </a>
-          <a href="https://linkfree.eddiehub.io/docs">
-            <li>Docs</li>
-          </a>
-        </ul>
+        <>
+          {widthScreen > 434 ? <SearchBar onSearch={handleSearch} /> : ''}
+          <ul
+            className='links'
+            style={{ listStyle: 'none', background: '#1a1a1b' }}
+          >
+            <a href='https://linkfree.eddiehub.io/events'>
+              <li>Events</li>
+            </a>
+            <a href='https://linkfree.eddiehub.io/map'>
+              <li>Map</li>
+            </a>
+            <a href='https://linkfree.eddiehub.io/docs'>
+              <li>Docs</li>
+            </a>
+          </ul>
+        </>
       ) : (
         <ul
-          className="links_view_mobile"
-          style={{ listStyle: "none", background: "#000d21" }}
+          className='links_view_mobile'
+          style={{
+            listStyle: 'none',
+            background: '#000d21',
+            maxHeight: '202px',
+            marginBottom: '5px',
+          }}
         >
           <hr></hr>
           <SearchBar onSearch={handleSearch} />
-          <a href="https://linkfree.eddiehub.io/events">
+          <a href='https://linkfree.eddiehub.io/events'>
             <li>Events</li>
           </a>
-          <a href="https://linkfree.eddiehub.io/map">
+          <a href='https://linkfree.eddiehub.io/map'>
             <li>Map</li>
           </a>
-          <a href="https://linkfree.eddiehub.io/docs">
+          <a href='https://linkfree.eddiehub.io/docs'>
             <li>Docs</li>
           </a>
         </ul>
       )}
-      <a className="button" onClick={toggleShowBar}>
-        <li style={{ listStyle: "none" }}>
-          <span className="pi pi-bars" style={{ fontSize: "20px" }}></span>
+      <a className='button' onClick={toggleShowBar}>
+        <li style={{ listStyle: 'none' }}>
+          <span className='pi pi-bars' style={{ fontSize: '20px' }}></span>
         </li>
       </a>
 
-      <img src="https://cdn-icons-png.flaticon.com/128/2130/2130631.png" alt="mode" className="darkmodeBtn" onClick={darkModeToggle} />
+      <img
+        src='https://cdn-icons-png.flaticon.com/128/2130/2130631.png'
+        alt='mode'
+        className='darkmodeBtn'
+        onClick={darkModeToggle}
+        style={
+          widthScreen > 690 ? { marginTop: '3.5px' } : { marginBottom: '4px' }
+        }
+      />
     </ul>
-  );
-};
+  )
+}
